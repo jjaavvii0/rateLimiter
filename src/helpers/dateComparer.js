41e7a62ca4216772ece929154f22fc124ecaf96c
unknown <jjaavvii0@gmail.com>
2022-10-20 18:42:21 +0100

    Code refactoring

--- src/helpers/dateComparer.js
+++ src/helpers/dateComparer.js
@@ -1,11 +1,32 @@
 const moment = require('moment')
 
 
-export function actualDateEqualStoragedDate(storagedData){  
+function actualDateEqualStoragedDate(storagedData){  
     if(!(storagedData == moment().format('MMMM Do YYYY, h a'))) return false
 }
 
-export function isLimitReached(storagedCount, type){  
+function isLimitReached(storagedCount, type){ 
     const typeOfLimit =  type==0 ? process.env.RATE_LIMIT_BY_TOKEN : process.env.RATE_LIMIT_BY_ID;
-    if(storagedCount++ > typeOfLimit) return true
+    if(storagedCount >= typeOfLimit) return true
+}
+
+export function dataCreator (storagedData, typeOfLimit){
+    if (storagedData == null){
+        let newUserData = {
+            'count': 1,
+            'startTime': moment().format('MMMM Do YYYY, h a')
+        }
+        return newUserData;
+    }
+    if (actualDateEqualStoragedDate(storagedData.startTime)){
+        storagedData.startTime = moment().format('MMMM Do YYYY, h a');
+        storagedData.count = 1;
+    }
+
+    if(isLimitReached(storagedData.count, typeOfLimit)){
+        storagedData.limitError = true;
+    }else{
+        storagedData.count++
+    }
+    return storagedData
 }
--- src/middlewares/rateLimitByToken.js
+++ src/middlewares/rateLimitByToken.js
@@ -1,47 +1,24 @@
 import {client, redlock} from "../databases/redis.db"
-import {actualDateEqualStoragedDate, isLimitReached} from "../helpers/dateComparer" 
+import {dataCreator} from "../helpers/dateComparer" 
 const moment = require('moment')
 
 export const checkLimit = async (req, res, next) =>{
-    client.exists(req.userId,(err,reply) => {
+    client.get(req.userId,async (err,reply) => {
         if(err) {
             console.log("Redis not working...")
             system.exit(0)
         }
-        // if(reply != 1)
-        if(reply === 1) {
-            client.get(req.userId,async (err,reply) => {
-                let data = JSON.parse(reply)
-                //BLOQUEO DEL RECURSO
-                await redlock.using(req.userId, 2000, async (signal) => {
-                    if (actualDateEqualStoragedDate(data.startTime)){
-                        data.startTime = moment().format('MMMM Do YYYY, h a');
-                        data.count = 1;
-                    }
-                    if(isLimitReached(data.count, 0)){
-                        data.count++;
-                        return res.status(429).json({
-                            "error": 1, 
-                            "message": "Limit reached, wait till '" + moment().add(1, 'hours').format('MMMM Do YYYY, ha') + "' to make a new request."
-                        })
-                    }else{
-                        data.count++
-                    }
-                    await client.set(req.userId, JSON.stringify(data))
-                    next();
-                    if (signal.aborted) {
-                      throw signal.error;
-                    }
+        let data = JSON.parse(reply)
+        await redlock.using(req.userId, 2000, async (signal) => {
+            const newData = dataCreator(data, 0)
+            if (newData.limitError){
+                return res.status(429).json({
+                    "error": 1, 
+                    "message": "Limit reached, wait till '" + moment().add(1, 'hours').format('MMMM Do YYYY, ha') + "' to make a new request."})
+            }
 
-                });
-            })
-        }else{
-            let data = {
-                'count': 1,
-                'startTime': moment().format('MMMM Do YYYY, h a')
-             }
-            client.set(req.userId,JSON.stringify(data))
-            next()
-        }
+            await client.set(req.userId, JSON.stringify(newData))
+            next();
+        });
     })
 }
